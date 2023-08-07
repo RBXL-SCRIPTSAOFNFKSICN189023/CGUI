7ef4c0a0948ec4f06a5854ebfe504df02043af8c
CGUI-RBXL <134892586+CGUI-RBXL@users.noreply.github.com>
2023-08-07 19:37:21 -0400

    Update secret.lua

--- secret.lua
+++ secret.lua
@@ -1,22 +1,18 @@
 local rem = require(game:GetService("ReplicatedStorage").Remotes)
 local robax = 0
 local exetick = tick()
-for i,v in pairs(game.Players.LocalPlayer.Character.Humanoid.RootPart:GetChildren()) do
-    if v.Name == "Spinning" then
-        v:Destroy()
-    end
-end
+print("Checkpoint 1")
 local sounds = {
   "rbxassetid://7188420724";
   "rbxassetid://9119637444";
-  "rbxassetid://5602666105";
   "rbxassetid://9113072098";
   "rbxassetid://5799014146";
   "rbxassetid://5810686185";
 }
 function troll()
     local sound = Instance.new("Sound", workspace)
-    sound.SoundId = sounds[math.random(1, #sounds)
+    sound.SoundId = sounds[math.random(1, #sounds)]
+    print(sound.SoundId)
     sound.Volume = 10
     repeat task.wait() until sound.IsLoaded
     sound:Play()
@@ -26,6 +22,7 @@ end
 troll()
 troll()
 troll()
+print("Checkpoint 2")
 rem.OnClientEvent("NotifyDonationParticipants"):Connect(function(from, to, ammount)
     if to == game.Players.LocalPlayer.Name or to == game.Players.LocalPlayer or to == game.Players.LocalPlayer.UserId then
         robax += ammount
@@ -36,6 +33,7 @@ rem.OnClientEvent("NotifyDonationParticipants"):Connect(function(from, to, ammou
     end
 end);
 
+print("Checkpoint 3")
 while task.wait(10) do
     print("made: " .. robax .. " robux by " .. tick() - exetick .. "s by the start of exe")
 end
