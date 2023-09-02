201b7de2513c8a5dd3736893380d7e21d012c4ac
RBXL-SCRIPTSAOFNFKSICN189023 <134892586+RBXL-SCRIPTSAOFNFKSICN189023@users.noreply.github.com>
2023-09-01 22:52:43 -0400

    Update 6516141723.lua

--- games/6516141723.lua
+++ games/6516141723.lua
@@ -1,1 +1,13 @@
+--[[_G.cLib = {
+  addCommand = addCommand;
+  newWindow = newWindow;
+  yesOrNo = yesOrNo;
+  numberInput = numberInput;
+  stringInput = stringInput;
+  optionList = optionList;
+  viewList = viewList;
+  notify = notify;
+  gui = gui;
+}]]
 
+_G.cLib.notify("Script only works in-game.")
