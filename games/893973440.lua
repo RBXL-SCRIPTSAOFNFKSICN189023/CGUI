941981b9dc3a1b6f7f7bd39d96e0aed702a71be2
RBXL-SCRIPTSAOFNFKSICN189023 <134892586+RBXL-SCRIPTSAOFNFKSICN189023@users.noreply.github.com>
2023-09-01 23:12:47 -0400

    Update 893973440.lua

--- games/893973440.lua
+++ games/893973440.lua
@@ -1,13 +1,13 @@
 --[[_G.cLib = {
-  addCommand = addCommand;
-  newWindow = newWindow;
-  yesOrNo = yesOrNo;
-  numberInput = numberInput;
-  stringInput = stringInput;
-  optionList = optionList;
-  viewList = viewList;
-  notify = notify;
-  gui = gui;
+  addCommand(command:string, alt:table<string>, exec:function):nil;
+  newWindow(title:string):window;
+  yesOrNo(title:string, message:string, callback:function<yes:bool>):window;
+  numberInput(title:string, message:string, callback:function<value:number>, rules:rulesNumber):window;
+  stringInput(title:string, message:string, callback:function<value:string>, maxCharacters:String):window;
+  optionList(title:string, optionList:table<string>, callback:function<optionN:number, option:string>):window;
+  viewList(title:string, optionList:table<string = string>):window;
+  notify(message:string):window;
+  gui:ScreenGui;
 }]]
 
 _G.cLib.scriptVersion = "v1.1B"
